4fbfe7c2516fa299286fc1be1cab54b42e4081d6
imran-abdou-souleiman-mohamed <imran.abdou-souleiman-mohamed@laplateforme.io>
2024-04-14 19:26:26 +0200

    carrousel fait mais il manque plusieurs chose a faire

--- Accueil/accueil.js
+++ Accueil/accueil.js
@@ -1,18 +1,51 @@
 document.body.onload = function(){
-    nbr = 5;
-    p=0;
-    container=document.getElementById("container");
-    g=document.getElementById("g");
-    d=document.getElementById("d");
-    container.style.width=(800*nbr)+"px";
-    for(i=l;i<=nbr;i++){
-        div=documents.createElement("div");
-        div.className="photo";
-        div.style.backgroundImage="url('images/im"+i+".jpg')";
+    var nbr = 5;
+    var p = 0;
+    var container = document.getElementById("container");
+    var g = document.getElementById("g");
+    var d = document.getElementById("d");
+    var containerWidth = 800 * nbr; // Largeur du conteneur
+    container.style.width = containerWidth + "px";
+    for(var i = 1; i <= nbr; i++){
+        var div = document.createElement("div");
+        div.className = "photo";
+        div.style.backgroundImage = "url('images/im" + i + ".jpg')";
         container.appendChild(div);
     }
-}
-g.onclick=function(){
-    p--;
-    container.style.tranforme="translate("+p*800+"px)";
+    afficherMasquer();
+};
+
+g.onclick = function(){
+    var p = parseInt(container.getAttribute('data-index')) || 0;
+    if(p > -4)
+        p--;
+    container.style.transform = "translateX(" + p * 800 + "px)";
+    container.setAttribute('data-index', p);
+    container.style.transition = "all 0.5s ease";
+    afficherMasquer();
+};
+
+d.onclick = function(){
+    var p = parseInt(container.getAttribute('data-index')) || 0;
+    if(p < 0) {
+        p++;
+        container.style.transform = "translateX(" + p * 800 + "px)";
+        container.setAttribute('data-index', p);
+        container.style.transition = "all 0.5s ease";
+        afficherMasquer();
+    } else {
+        // Empêche le défilement supplémentaire
+    }
+};
+
+function afficherMasquer(){
+    var p = parseInt(container.getAttribute('data-index')) || 0;
+    if(p == -2)
+        g.style.visibility = "hidden";
+    else 
+        g.style.visibility = "visible";
+    if(p == 0)
+        d.style.visibility = "hidden";
+    else 
+        d.style.visibility = "visible";
 }
